b92b0754aaf9a1071818970de91e1d5624a2d71e
Tirumala Kashibhatla <kashi_sudheer@yahoo.com>
2019-07-20 04:28:00 +0000

    App changes - Tirumala

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,26 +5,25 @@ sap.ui.define([
 	"use strict";
 
 	return BaseController.extend("demo.oft.zoft_etag_support.controller.App", {
-
-		onInit : function () {
+		onInit: function () {
 			var oViewModel,
 				fnSetAppNotBusy,
 				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
 
 			oViewModel = new JSONModel({
-				busy : true,
-				delay : 0
+				busy: true,
+				delay: 0
 			});
 			this.setModel(oViewModel, "appView");
 
-			fnSetAppNotBusy = function() {
+			fnSetAppNotBusy = function () {
 				oViewModel.setProperty("/busy", false);
 				oViewModel.setProperty("/delay", iOriginalBusyDelay);
 			};
 
 			// disable busy indication when the metadata is loaded and in case of errors
 			this.getOwnerComponent().getModel().metadataLoaded().
-				then(fnSetAppNotBusy);
+			then(fnSetAppNotBusy);
 			this.getOwnerComponent().getModel().attachMetadataFailed(fnSetAppNotBusy);
 
 			// apply content density mode to root view
